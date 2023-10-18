8aa6dad2f6bbb6bc5f425067faf0f1c8572ff1e2
eliasmr98 <eliasmartinrivas@gmail.com>
2023-10-18 22:07:39 +0200

    Create functions in tools file and testing it

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,1 +1,27 @@
+export const arrayLength = (array) => {
+  let count = 0;
+  // eslint-disable-next-line no-unused-vars
+  for (const _item of array) {
+    count++;
+  }
 
+  return count;
+};
+
+export const arrayPush = (array) => {
+  const arrayLengthValue = arrayLength(array);
+  const arrayPushValue = arrayLengthValue + 1;
+  return arrayPushValue;
+};
+
+export const arrayPop = (array) => {
+  const arrayPopLast = array[array.length - 1];
+  const arrayPopValue = arrayPopLast;
+  return arrayPopValue;
+};
+
+export const arrayShift = (array) => {
+  const arrayShiftFirst = array[0];
+  const arrayShiftValue = arrayShiftFirst;
+  return arrayShiftValue;
+};
--- scripts/tools.test.js
+++ scripts/tools.test.js
@@ -1,1 +1,96 @@
+const { arrayLength } = require('./tools');
+const { arrayPush } = require('./tools');
+const { arrayPop } = require('./tools');
+const { arrayShift } = require('./tools');
 
+describe('Given arrayLength', () => {
+  test('Empty array, result should be 0', () => {
+    const testData = [];
+    const expected = 0;
+    const result = arrayLength(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('3 items array, should be 3', () => {
+    const testData = [1, 4, 7];
+    const expected = 3;
+    const result = arrayLength(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('3 items array, should be 3', () => {
+    const testData = [1, null, undefined];
+    const expected = 3;
+    const result = arrayLength(testData);
+    expect(result).toBe(expected);
+  });
+});
+
+describe('Given arrayPush', () => {
+  test('Empty array, result should be 1', () => {
+    const testData = [];
+    const expected = 1;
+    const result = arrayPush(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('Array length 3, should be 4', () => {
+    const testData = [1, 4, 7];
+    const expected = 4;
+    const result = arrayPush(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('3 items array, should be 4', () => {
+    const testData = [1, null, undefined];
+    const expected = 4;
+    const result = arrayPush(testData);
+    expect(result).toBe(expected);
+  });
+});
+
+describe('Given arrayPop', () => {
+  test('Empty array, result should be Pepe', () => {
+    const testData = ['Luis', 'Sergio', 'Pepe'];
+    const expected = 'Pepe';
+    const result = arrayPop(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('Numbers array, result should be 7', () => {
+    const testData = [1, 4, 7];
+    const expected = 7;
+    const result = arrayPop(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('3 items array, should be undefined', () => {
+    const testData = [1, null, undefined];
+    const expected = undefined;
+    const result = arrayPop(testData);
+    expect(result).toBe(expected);
+  });
+});
+
+describe('Given arrayShift', () => {
+  test('Empty array, result should be Luis', () => {
+    const testData = ['Luis', 'Sergio', 'Pepe'];
+    const expected = 'Luis';
+    const result = arrayShift(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('Numbers array, result should be 1', () => {
+    const testData = [1, 4, 7];
+    const expected = 1;
+    const result = arrayShift(testData);
+    expect(result).toBe(expected);
+  });
+
+  test('3 items array, should be 1', () => {
+    const testData = [1, null, undefined];
+    const expected = 1;
+    const result = arrayShift(testData);
+    expect(result).toBe(expected);
+  });
+});
