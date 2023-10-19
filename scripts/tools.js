6c197c74634108d069946aef108b814865db494b
eliasmr98 <eliasmartinrivas@gmail.com>
2023-10-19 22:06:57 +0200

    Correct push an pop functions with teachers explnations

--- scripts/tools.js
+++ scripts/tools.js
@@ -8,20 +8,39 @@ export const arrayLength = (array) => {
   return count;
 };
 
-export const arrayPush = (array) => {
-  const arrayLengthValue = arrayLength(array);
-  const arrayPushValue = arrayLengthValue + 1;
-  return arrayPushValue;
+// GRACIAS NITIN
+export const arrayPush = (array, ...pushedElement) => {
+  const elementsToPush = [...pushedElement];
+
+  for (let index = 0; index < elementsToPush.length; index++) {
+    array[array.length] = elementsToPush[index];
+  }
+
+  return array.length;
 };
+// GRACIAS NITIN
 
 export const arrayPop = (array) => {
-  const arrayPopLast = array[array.length - 1];
-  const arrayPopValue = arrayPopLast;
+  if (array.length === 0) return undefined;
+
+  const arrayPopValue = array[array.length - 1];
+
+  array.length -= 1;
+
   return arrayPopValue;
 };
 
 export const arrayShift = (array) => {
-  const arrayShiftFirst = array[0];
-  const arrayShiftValue = arrayShiftFirst;
-  return arrayShiftValue;
+  if (array.length === 0) return undefined;
+
+  const arrayParameter = array[array[1]];
+  arrayParameter.length -= 1;
+
+  return arrayParameter;
+
+  // REAL RETURN return array[0];
 };
+
+console.log(arrayShift(['Pepe', 'Luis', 'Kamilo']));
+
+console.log([].shift());
